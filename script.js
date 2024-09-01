672ba2d8e69874adf1dad423fa4e163b43dbb640
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-09-01 19:51:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+
+function button1()
+{
+
+document.getElementById("para1").innerHTML = "hello I am Rehan I like to play cricket"
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function add1(a,b)
+{
+
+var sum = (a+b)
+document.getElementById("add2").innerHTML =sum;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
